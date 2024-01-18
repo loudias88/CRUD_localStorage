3ae966aff03a1259a3af3da5599fa476afbb3eb3
loudias88 <luisheleno.dias@gmail.com>
2024-01-18 11:38:46 -0300

    first commit

---
+++ server.py
@@ -0,0 +1,20 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+
+class MyHttpRequestHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return super().do_GET()
+
+# Set the directory where the HTML file is located
+os.chdir(os.path.expanduser('~/Área de Trabalho/Projects/python-lab/CRUD'))
+
+# Server settings
+server_address = ('localhost', 8000)
+httpd = HTTPServer(server_address, MyHttpRequestHandler)
+
+# Run the server
+print("Server running on port 8000...")
+httpd.serve_forever()
+
